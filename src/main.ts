90e18f50a05fa7d425997a1cbc7101f983472131
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-11-01 10:37:10 +0100

    :broom: Reduced and cleaned script

--- src/main.ts
+++ src/main.ts
@@ -1,160 +1,128 @@
 import * as type from "./types"
 
 function minerStats(json: type.MinerAPI) {
-	function average() {
-		let unix = Date.now(),
-			yesterday = 0,
-			daybefore = 0,
-			jour = json.sumrewards[2].reward
+	function payoutDates() {
+		function calcAverage() {
+			let unix = Date.now(),
+				yesterday = 0,
+				daybefore = 0,
+				jour = json.sumrewards[2].reward
 
-		//calc le nbr de jours avant reward
-		const ratio = (by: number) => (0.05 - sat(json.stats.balance)) / sat(by)
+			//calc le nbr de jours avant reward
+			const ratio = (by: number) =>
+				(0.05 - sat(json.stats.balance)) / sat(by)
 
-		//increment les rewards comprise entre les intervalles
-		const incrRewards = (
-			interval: number[],
-			re: { timestamp: number; reward: number }
-		) =>
-			re.timestamp < unix - interval[0] &&
-			re.timestamp > unix - interval[1]
-				? re.reward
-				: 0
+			//increment les rewards comprise entre les intervalles
+			const incrRewards = (
+				interval: number[],
+				re: { timestamp: number; reward: number }
+			) =>
+				re.timestamp < unix - interval[0] &&
+				re.timestamp > unix - interval[1]
+					? re.reward
+					: 0
 
-		for (let re of json.rewards) {
-			yesterday += incrRewards([86400, 172800], re)
-			daybefore += incrRewards([172800, 259200], re)
-		}
-
-		const calcMoyenne = (array: number[]) => {
-			let average: number[] = []
-
-			array.forEach((temps) => {
-				if (temps > 0) {
-					average.push(ratio(temps))
-				}
-			})
-
-			if (average.length > 0) {
-				return average.reduce((a, b) => a + b) / average.length
-			} else {
-				return 0
+			for (let re of json.rewards) {
+				yesterday += incrRewards([86400, 172800], re)
+				daybefore += incrRewards([172800, 259200], re)
 			}
+
+			const calcMoyenne = (array: number[]) => {
+				let average: number[] = []
+
+				array.forEach((temps) => {
+					if (temps > 0) {
+						average.push(ratio(temps))
+					}
+				})
+
+				if (average.length > 0) {
+					return average.reduce((a, b) => a + b) / average.length
+				} else {
+					return 0
+				}
+			}
+
+			return calcMoyenne([daybefore, yesterday, jour])
 		}
 
-		return calcMoyenne([daybefore, yesterday, jour])
+		function countdown(d: { jour: number; heure: number }) {
+			const pluriel = (num: number, str: string[]) =>
+				num === 1
+					? `${num} ${str[0]}`
+					: num > 1
+					? `${num} ${str[1]}`
+					: ``
+
+			return (
+				pluriel(d.jour, ["jour", "jours"]) +
+				(d.jour > 0 && d.heure > 0 ? " et " : "") +
+				pluriel(d.heure, ["heure", "heures"])
+			)
+		}
+
+		function date(d: { jour: number; heure: number }) {
+			//fait avancer la date
+			let payout = new Date()
+			payout.setDate(payout.getDate() + d.jour)
+			payout.setHours(payout.getHours() + d.heure)
+
+			return localeDate(payout)
+		}
+
+		const dom = {
+			restant: id("jourrestant"),
+			date: id("date"),
+			titre: id("titre"),
+		}
+
+		if (json.hashrate > 0) {
+			//calcule les moyennes
+			const average = calcAverage()
+			const restant = {
+				jour: Math.floor(average),
+				heure: Math.floor((average * 24) % 24),
+			}
+
+			//ajoute les dates
+			dom.titre.innerText = "Tu seras payé"
+			dom.restant.innerText = "Dans " + countdown(restant)
+			dom.date.innerText = "Le " + date(restant)
+
+			//affiche la partie payouts
+			dom.restant.classList.add("shown")
+			dom.date.classList.add("shown")
+		} else {
+			dom.titre.innerText = "Tu ne mines pas"
+		}
 	}
 
-	function countdown(d: type.RestantDate) {
-		const pluriel = (num: number, str: string[]): string => {
-			return num === 1
-				? `${num} ${str[0]}`
-				: num > 1
-				? `${num} ${str[1]}`
-				: ``
-		}
-
-		return (
-			"Dans " +
-			pluriel(d.jour, ["jour", "jours"]) +
-			(d.jour > 0 && d.heure > 0 ? " et " : "") +
-			pluriel(d.heure, ["heure", "heures"])
-		)
-	}
-
-	function date(d: type.RestantDate) {
-		//fait avancer la date
-		let laterDate = new Date()
-		laterDate.setDate(laterDate.getDate() + d.jour)
-		laterDate.setHours(laterDate.getHours() + d.heure)
-
-		const addTenZero = (num: number): string =>
-			num < 10 ? "0" + num.toString() : num.toString()
-
-		//enregistre les formats a afficher
-		let ldObj = {
-			mo: mois[laterDate.getMonth()],
-			j: laterDate.getDate(),
-			h: addTenZero(laterDate.getHours()),
-			mi: addTenZero(laterDate.getMinutes()),
-		}
-
-		return `Le ${ldObj.j} ${ldObj.mo} à ${ldObj.h}:${ldObj.mi}`
+	function toMinerPage() {
+		const url = `https://eth.2miners.com/account/${ADDRESS}`
+		id("eth").innerText = (json.stats.balance * 10 ** -9).toFixed(3)
+		id("minerLink").setAttribute("href", url)
 	}
 
 	function history() {
 		const historyDom = document.querySelector(".history-liste")!
 
 		json.payments.forEach((pay) => {
-			const payDate = new Date(pay.timestamp * 1000)
 			const p = document.createElement("p")
-
-			p.innerText = payDate.toLocaleDateString("fr-fr", {
-				day: "2-digit",
-				month: "long",
-				hour: "2-digit",
-				minute: "2-digit",
-			})
-
+			p.innerText = localeDate(new Date(pay.timestamp * 1000))
 			historyDom.appendChild(p)
 		})
 	}
 
-	const mois = [
-		"janvier",
-		"fevrier",
-		"mars",
-		"avril",
-		"mai",
-		"juin",
-		"juillet",
-		"août",
-		"septembre",
-		"octobre",
-		"novembre",
-		"decembre",
-	]
+	const localeDate = (d: Date) =>
+		d.toLocaleDateString("fr-fr", {
+			day: "2-digit",
+			month: "long",
+			hour: "2-digit",
+			minute: "2-digit",
+		})
 
-	const jours = [
-		"Lundi",
-		"Mardi",
-		"Mercredi",
-		"Jeudi",
-		"Vendredi",
-		"Samedi",
-		"Dimanche",
-	]
-
-	//liste les doms à afficher
-	const dom = {
-		titre: document.querySelector("h1")!,
-		jourrestant: id("jourrestant"),
-		date: id("date"),
-	}
-
-	//liste les dates a utiliser
-	const moyenne = average()
-	const restant = {
-		jour: Math.floor(moyenne),
-		heure: Math.floor((moyenne * 24) % 24),
-	}
-
-	//modifie le DOM en fonction de la moyenne trouvé
-	if (json.hashrate === 0 || moyenne === 0) {
-		dom.titre.innerText = "Tu ne mines pas"
-		dom.jourrestant.style.display = "none"
-		dom.date.style.display = "none"
-	} else {
-		dom.titre.innerText = "Tu seras payé"
-		dom.jourrestant.innerText = countdown(restant)
-		dom.date.innerText = date(restant)
-	}
-
-	id("eth").innerText = (json.stats.balance * 10 ** -9).toFixed(3)
-	id("minerLink").setAttribute(
-		"href",
-		`https://eth.2miners.com/account/${ADDRESS}`
-	)
-
+	toMinerPage()
+	payoutDates()
 	history()
 }
 
